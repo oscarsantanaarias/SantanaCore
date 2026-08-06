dc1e12b259dce8d456212c856d5a9b7517f844de
oscar <oscar.atr20003@hotmail.com>
2026-08-05 20:06:03 -0400

    Drop the two legacy game server projects

--- src/GameServerLastest/Game/Events.cs
+++
@@ -1,4 +0,0 @@
-
-namespace Santana.Game
-{
-}
--- src/GameServerLastest/Resource/Experience.cs
+++
@@ -1,9 +0,0 @@
-namespace Santana.Resource
-{
-  public class Experience
-  {
-    public int Level { get; set; }
-    public uint ExperienceToNextLevel { get; set; }
-    public uint TotalExperience { get; set; }
-  }
-}
